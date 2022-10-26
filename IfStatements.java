149a8a17d6336ab05e904577783f56d9ec068829
Ewa Maj <36788526+EwaWisimito@users.noreply.github.com>
2022-10-26 14:35:09 +0200

    flagi

--- IfStatements.java
+++ IfStatements.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class IfStatements {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        boolean correctOperation = false;
+
         System.out.print("Podaj pierwszą liczbę: ");
         int a = scanner.nextInt();
         System.out.print("Podaj drugą liczbę: ");
@@ -19,20 +21,28 @@ public class IfStatements {
         }
         if(operation == '+'){
             System.out.println("wynik dodawania to: " + (a+b));
+            correctOperation = true;
         }
         if(operation == '-'){
             System.out.println("wynik odejmowania to: " + (a-b));
+            correctOperation = true;
         }
         if(operation == '*'){
             System.out.println("wynik mnożenia to: " + (a*b));
+            correctOperation = true;
         }
         if(operation == '/'){
             if(b!=0){
                 System.out.println("wynik dzielenia to: " + ((1.0*a/b)));
+                correctOperation = true;
             } 
             else{
                 System.out.println("Tak nie wolno!");
             }
+        }
+        if(!correctOperation){
+            System.out.print("Jednorożec");
+
         }
     }
 }
