56f3d75452d6fc03486c8bed60fe2114542a93df
Ewa Maj <36788526+EwaWisimito@users.noreply.github.com>
2022-10-26 12:21:58 +0200

    next

---
+++ VariableTypes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class VariableTypes {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println("Hello World: " + input);
+
+
+    }
+}
+
