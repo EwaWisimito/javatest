d161222605d2dbd9a654fe9af2f1621dada5d1de
Ewa Maj <36788526+EwaWisimito@users.noreply.github.com>
2022-10-27 10:53:21 +0200

    for

---
+++ Loops1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Loops1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int i = 0;
+
+        while(i<1){
+            String name =scanner.next();
+            System.out.println("hello" + name);
+            i++;
+        }
+
+       
+    }
+ 
+}
---
+++ Loops2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loops2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Wpisz liczbę: ");
+        int a = scanner.nextInt();
+
+     for(int i= a; i<=50; i++){
+        System.out.println(i);
+     }
+
+       
+    }
+ 
+}
